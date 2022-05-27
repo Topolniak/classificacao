38e5d8ecd6f477f589aca6fac00c4cd06f15139c
Luciano Topolniak <Topolniak@gmail.com>
2022-05-27 10:34:53 -0400

    Update classificacao.py

--- classificacao.py
+++ classificacao.py
@@ -10,30 +10,28 @@ modelo = pickle.load(Arquivo)
 
 st.set_page_config(layout="wide")
 
-st.title("Prevendo o Status dos Alunos")
+st.title("Previsão de status de aprovaçao ou reprovação de alunos.")
 
-st.subheader("**Classificação com Random Forest**")
-st.markdown("Este app faz a classificação do status do aluno: Aprovado/Reprovado.")
+st.subheader("**Classificação usando Random Forest**")
+st.markdown("Este aplicativo realiza a previsão do status final de alunos: Aprovado/Reprovado.")
 
 st.sidebar.subheader('***Selecione as modalidades:***')
 
 # Carrega do dataframe de forma dinâmica
-dados = st.file_uploader('Escolha o dataset (.csv) para continuarmos com as análises: ', type='csv')
+dados = st.file_uploader('Escolha o arquivo de dados (.csv) que deseja usar para a análise: ', type='csv')
 if dados is not None:
     dfBase = pd.read_csv(dados)
     #AgGrid(dfBase)  # interativo
 
-    st.write("Shape: ", dfBase.shape)
+    st.write("Sobre o arquivo: ", dfBase.shape)
 
-    st.markdown("Quantidade de alunos por status")
+    st.markdown("Percentual de alunos por status:")
     dados = dfBase['status'].value_counts(normalize=True) * 100
     st.dataframe(dados)
 
     df1 = dfBase.groupby(["status"]).count().reset_index()
     grfStatus = px.bar(df1,
-                       y=dfBase[['status']].groupby(['status'])['status'].count() / dfBase.shape[0] * 100,
-                       x="status",
-                       color='status')
+                       y=dfBase[['status']].groupby(['status'])['status'].count() / dfBase.shape[0] * 100, x="status", color='status')
     st.plotly_chart(grfStatus)
 
     # Inicio do Random Forest
@@ -53,17 +51,17 @@ if dados is not None:
 
     # Juntando valores preditos com base original
     prev = previsao
-    prev = pd.DataFrame(prev, columns=['Status Predict'])
+    prev = pd.DataFrame(prev, columns=['Status Predito'])
     cont = 0
-    for i in prev['Status Predict']:
+    for i in prev['Status Predito']:
         if i == 0:
-            prev['Status Predict'][cont] = "Reprovado"
+            prev['Status Predito'][cont] = "Reprovado"
         else:
-            prev['Status Predict'][cont] = "Aprovado"
+            prev['Status Predito'][cont] = "Aprovado"
         cont += 1
     base_concat = pd.concat([dfBase, prev], verify_integrity=True, axis=1)
 
-    st.markdown("Quantidade Predict de alunos por status")
+    st.markdown("Quantidade de alunos por status predito.")
     st.markdown(f'Aprovados: {Aprovados}')
     st.markdown(f'Reprovados: {Reprovados}')
 
@@ -72,6 +70,9 @@ if dados is not None:
     A = (Aprovados / T) * 100
     R = (Reprovados / T) * 100
 
-    st.markdown(f'Aprovados: {round(A, 4)} %')
-    st.markdown(f'Reprovados: {round(R, 4)} %')
+    st.markdown(f'Aprovados: {round(A, 4):.2f} %')
+    st.markdown(f'Reprovados: {round(R, 4):.2f} %')
+    st.subheader("**Comparação entre o fato e o predito.**")
+    st.markdown("Verifique na tabela abaixo as colunas no final que informam o que de fato aconteceu com o aluno e o que foi predito para o o mesmo.")
     AgGrid(base_concat)
+#%%
